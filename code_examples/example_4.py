7bc90063dabfa671039417d94b49e2ee0a87fa76
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-10 09:08:00 -0600

    Example added.

---
+++ code_examples/example_4.py
@@ -0,0 +1,3 @@
+async def my_async_function(a: int, b: list[bool]) -> 42:
+    x: int = 1
+    y: bool
--- gradual_untyping/__main__.py
+++ gradual_untyping/__main__.py
@@ -4,12 +4,12 @@ Temp file for testing purposes.
 from pathlib import Path
 from .untype import untype
 
-NEXAMPLES = 3
+NEXAMPLES = 4
 EXAMPLES_PATH = Path("code_examples")
 
 for n in range(1, NEXAMPLES + 1):
     code_path = EXAMPLES_PATH / f"example_{n}.py"
     code = code_path.read_text()
 
-    cleaned_code_path = EXAMPLES_PATH / f"example_{n}(cleaned).py"
+    cleaned_code_path = EXAMPLES_PATH / f"example_{n}(untyped).py"
     cleaned_code_path.write_text(untype(code))
