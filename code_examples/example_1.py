3ab62ab7372c5d53a3db7f3ea3f776d8bc922f34
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-09 22:20:36 -0600

    Initial commit.

---
+++ code_examples/example_1.py
@@ -0,0 +1,15 @@
+x: int
+y: bool
+
+class MyClass:
+    x: int
+    y: bool
+
+    def my_method(self, x: int, y: bool) -> 42:
+        z: int
+        a: bool
+
+
+def my_function(x: int, y: bool) -> 42:
+    z: int
+    a: bool
---
+++ gradual_untyping/replacement.py
@@ -0,0 +1,9 @@
+from typing import NamedTuple
+
+
+class Replacement(NamedTuple):
+    lineno: int
+    col_offset: int
+    end_lineno: int
+    end_col_offset: int
+    find_preceding_colon: bool
