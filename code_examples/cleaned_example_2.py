384db0e0b90b081b4438796f31532cd9c559da25
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-10 04:23:01 -0600

    Initial commit.

---
+++ code_examples/cleaned_example_2.py
@@ -0,0 +1,11 @@
+
+def f(b,
+    c,
+):
+    pass
+
+def g(d):
+    pass
+
+def h(e):
+    pass
---
+++ code_examples/example_2.py
@@ -0,0 +1,20 @@
+a: """
+multiline annotation
+"""
+
+def f(b: """
+    multiline annotation
+    """,
+    c,
+):
+    pass
+
+def g(d) -> """
+    multiline annotation
+""":
+    pass
+
+def h(e):
+    i: """
+    multiline annotation
+    """
