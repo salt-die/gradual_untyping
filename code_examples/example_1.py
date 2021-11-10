3678c3ab901f86cbf17dd99d87518af85c7dcbc8
salt-die <53280662+salt-die@users.noreply.github.com>
2021-11-09 23:18:57 -0600

    Ordered annotations for easier debugging.

--- code_examples/example_1.py
+++ code_examples/example_1.py
@@ -1,15 +1,27 @@
-x: int
-y: bool
+from dataclasses import dataclass
+from typing import NamedTuple
+
+a: 1
+b: 2
 
 class MyClass:
-    x: int
-    y: bool
+    c: 3
+    d: 4
 
-    def my_method(self, x: int, y: bool) -> 42:
-        z: int
-        a: bool
+    def my_method(self, e: 5, f: 6) -> 7:
+        g: 8
+        h: 9
 
 
-def my_function(x: int, y: bool) -> 42:
-    z: int
-    a: bool
+def my_function(i: 10, j: 11) -> 12:
+    k: 13
+    l: 14
+
+class MyTuple(NamedTuple):
+    m: 15
+    n: 16
+
+@dataclass
+class MyDataClass:
+    o: 17
+    p: 18
